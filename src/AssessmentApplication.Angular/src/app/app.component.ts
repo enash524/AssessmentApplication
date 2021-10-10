2b7885d1919c130e7b9de6170c4c7d08544d8707
enash524 <54128752+enash524@users.noreply.github.com>
2021-10-09 23:42:47 -0500

    1) Fixed nav bar to bottom of screen

--- src/AssessmentApplication.Angular/src/app/app.component.ts
+++ src/AssessmentApplication.Angular/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   private title: string = 'Assessment Application';
