d3c8684ed9fb140a9ae83a2fed1a22126d72b405
enash524 <54128752+enash524@users.noreply.github.com>
2022-02-02 00:35:41 -0600

    1) Add ability to debug karma tests 2) Add additional path shortcuts

--- src/AssessmentApplication.Angular/karma.conf.js
+++ src/AssessmentApplication.Angular/karma.conf.js
@@ -37,8 +37,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeDebug'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeDebug: {
+        base: 'Chrome',
+        flags: ['--remote-debugging-port=9333']
+      }
+    },
   });
 };
