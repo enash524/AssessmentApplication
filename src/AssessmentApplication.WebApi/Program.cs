aeab71502425acfa2af56192d90a8da6ff420b8b
enash524 <54128752+enash524@users.noreply.github.com>
2025-11-27 22:32:36 -0600

    1) Updated installed packages to latest version 2) Target .net 10

--- src/AssessmentApplication.WebApi/Program.cs
+++ src/AssessmentApplication.WebApi/Program.cs
@@ -23,7 +23,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 
 string _policyName = "CorsPolicy";
 
